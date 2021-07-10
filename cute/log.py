b977af37229866ea0344ce49c7d88838798a815d
onioneffect <admin@explosive.love>
2021-07-10 11:42:12 -0400

    Makes cute\log.py look nicer

--- cute/log.py
+++ cute/log.py
@@ -1,9 +1,11 @@
 import logging
 
-fmt = "[%(asctime)s] %(message)s"
-date_format = "%Y-%m-%d %I:%M:%S %p"
-
 def init_log(mode : int = logging.INFO):
-	logging.basicConfig(level=mode, format=fmt, datefmt=date_format)
+	logging.basicConfig(
+		level=mode,
+		format="[%(asctime)s] %(message)s",
+		datefmt="%Y-%m-%d %I:%M:%S %p"
+	)
+
 	return 1
 
